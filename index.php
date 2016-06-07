07d594bb366401e69ae0a2b70fd5c2d4169dcb4c
QuantumSoul <QuantumSoul@caramail.fr>
2016-06-07 23:27:48 +0200

    création de la page de connexion

--- index.php
+++ index.php
@@ -5,15 +5,33 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-        <script src="/js/bootstrap.min.js"></script>
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
         
         <link rel="stylesheet" type="text/css" href="css/custom.css">
+        <style type="text/css">
+            #valider {
+                margin-top: 5%;
+                margin-left: auto;
+                margin-right: auto;
+                width: 75%;
+                height: auto;
+                border-radius: 17px;
+                font-size: 300%;
+            }
+        </style>
     </head>
     <body>
         <?php include_once('session.php'); ?>
-        <section class="container">
-            <div class="col-lg-8 col-sm-offset-2 mainDisplay" >
-                
+        <section class="container-fluid">
+            <div class="col-sm-12 col-xs-12 col-lg-12 mainDisplay">
+                <form class="form-group">
+                    <label for="username">Nom d'utilisateur:</label>
+                    <input type="text" id="username" class="form-control" />
+                    <label for="password">Mot de passe:</label>
+                    <input type="password" id="password" class="form-control" />
+                    <button id="valider" class="btn btn-success">Se connecter</button>
+                </form>
             </div>
         </section>
     </body>
