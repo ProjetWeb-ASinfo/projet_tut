23a7b078063bbce0238299018503df5828074696
QuantumSoul <QuantumSoul@caramail.fr>
2016-06-09 13:09:40 +0200

    fin de la page cartes de l'utilisateur

--- connexion.php
+++ connexion.php
@@ -2,9 +2,9 @@
     <div class="col-sm-12 col-xs-12 col-lg-12 connexionScreen">
         <div class="form-group">
             <label for="username">Nom d'utilisateur:</label>
-            <input type="text" id="username" class="form-control" />
+            <input type="text" id="username" class="form-control" value="" />
             <label for="password">Mot de passe:</label>
-            <input type="password" id="password" class="form-control" />
+            <input type="password" id="password" class="form-control" value="" />
             <button id="valider" class="btn btn-success">Se connecter</button>
         </div>
         <script>
--- index.php
+++ index.php
@@ -12,22 +12,63 @@
         <link rel="stylesheet" type="text/css" href="css/custom.css">
     </head>
     <body>
-        <div id="bg"></div>
         <script>
             function charger(path) {
                 $("#content").css("transform", "scale(0,0)");
                 setTimeout(function() {
                     $("#content").load(path);
                     $("#content").css("transform", "scale(1,1)");
+                    $("#content").css("transform", "");
                 }, 500);
             }
         </script>
-        <div id="content">
-            <?php
-                if ($_SESSION['users']['id']=='')
-                    include 'connexion.php';
-            ?>
+        <div id="bg">
+            <div id="content">
+                <?php
+                    if ($_SESSION['users']['id']=='')
+                        //include 'connexion.php';
+                ?>
+                <div class="title"><h1>Mes cartes:</h1></div>
+                <section class="container-fluid">
+                    <div class="col-xs-12 col-sm-12 col-md-12" id="header-table">
+                        <table class="table">
+                            <thead>
+                                    <tr>
+                                        <th>Nom</th>
+                                        <th>Type</th>
+                                        <th>Image</th>
+                                    </tr>
+                            </thead>
+                        </table>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12" id="card-table">
+                        <table class="table">
+                            <tbody>
+                                <?php
+                                    for ($i=1; $i<10; $i++) {
+                                        echo '<tr>';
+                                        echo "<td>Carte $i</td>"
+                                            . "<td>Type $i</td>"
+                                            . "<td><a id='$i' href='#modal-preview' data-toggle='modal'><img src='cards/card$i.png' alt='carte$i'></a></td>";
+                                        echo '</tr>';
+                                    }
+                                ?>
+                            </tbody>
+                        </table>
+                        <script>
+                            $(".table a").click(function() {
+                                $("#preview").attr("src", "cards/card"+$(this).attr("id")+".png");
+                                $("#preview").attr("alt", "carte "+$(this).attr("id"));
+                            });
+                        </script>
+                    </div>
+                    <div id="modal-preview" class="modal fade">
+                        <div class="modal-dialog">
+                            <img id="preview" src="" alt="">
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
-<div></div>
     </body>
 </html>
